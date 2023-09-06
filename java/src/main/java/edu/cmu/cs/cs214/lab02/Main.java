ba91c22cea05be1871b5b3e5a829f3a7b1d4b208
Jiatong Li <jiatongli222@jiatongs-mbp-2.wifi.local.cmu.edu>
2023-09-05 21:25:04 -0400

    java fixed with encapsulation v1

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,34 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Rectangle; 
+import edu.cmu.cs.cs214.lab02.shapes.Circle; 
+import edu.cmu.cs.cs214.lab02.shapes.Square; 
+
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        // rectangle
+        Shape rectangle = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer rendererRect = new Renderer(rectangle);
 
-        renderer.draw();
+        rendererRect.draw();
+
+        //circle
+        Shape circle = new Circle(5); 
+
+        Renderer rendererCircle = new Renderer(circle); 
+
+        rendererCircle.draw(); 
+
+        //square 
+        Shape square = new Square(4); 
+
+        Renderer rendererSq = new Renderer(square); 
+
+        rendererSq.draw(); 
+
+
     }
 }
